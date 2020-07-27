b52c89128a66a356479083b5f18bd8744b916e03
enriqueuz <enrique.uzcategui@outlook.com>
2020-07-27 17:10:37 -0400

    User registration implemented

--- gym/settings.py
+++ gym/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
 
     # Gym custom apps
     'payment.apps.PaymentConfig',
-    'users.apps.UsersConfig'
+    'users.apps.UsersConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- gym/urls.py
+++ gym/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('payment/', include('payment.urls')),
+    path('login/', auth_views.LoginView.as_view(), name='Login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='Logout'),
+
 ]
